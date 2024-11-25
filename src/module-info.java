dad3991b6a4a1d5541a82f736db7f8eca75cdfe9
Rayane Rodrigues <rayane.rodrigues.1@academico.ifpb.edu.br>
2024-11-25 18:11:29 -0300

    chore: atualização das dependências

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,11 @@
  * 
  */
 module Catalogo_de_Livros {
+    requires jakarta.persistence;
+    requires jakarta.servlet;
+    
+    requires java.sql;
+    requires org.hibernate.orm.core;
+
+    opens main.java.model to org.hibernate.orm.core;
 }
