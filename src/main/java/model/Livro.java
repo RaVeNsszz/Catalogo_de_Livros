d5b771791443f0ea84d0c830fe5c9e3f32fcd4bd
Rayane Rodrigues <rayane.rodrigues.1@academico.ifpb.edu.br>
2024-11-25 18:12:43 -0300

    feat: criação do model

--- src/main/java/model/Livro.java
+++ src/main/java/model/Livro.java
@@ -1,5 +1,86 @@
 package main.java.model;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "livros")
 public class Livro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String titulo;
+
+	@Column(nullable = false)
+	private String autor;
+
+	private String genero;
+
+	@Column(nullable = false, unique = true)
+	private String isbn;
+
+	private LocalDate dataPublicacao;
+
+	private BigDecimal preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public LocalDate getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(LocalDate dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
 }
